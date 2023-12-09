2d92a0812fc9b139855437d33920944c8f0aef0e
RudinP <song_e@sookmyung.ac.kr>
2023-12-09 15:38:18 +0900

    노래목록 테이블뷰 구현

---
+++ BearPlay/MusicTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MusicTableViewCell.swift
+//  BearPlay
+//
+//  Created by 루딘 on 12/9/23.
+//
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var lengthLabel: UILabel!
+    @IBOutlet weak var albumImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BearPlay/ViewController.swift
+++ BearPlay/ViewController.swift
@@ -7,23 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource{
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var musicTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let searchController = UISearchController(searchResultsController: nil)
         self.navigationItem.searchController = searchController
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        
+        musicTableView.dataSource = self
     }
 }
 
-extension ViewController{
+extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return Music.dummyMusicList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "musicCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "musicCell", for: indexPath) as! MusicTableViewCell
+        
+        cell.titleLabel.text = Music.dummyMusicList[indexPath.row].title
+        cell.lengthLabel.text = String(Music.dummyMusicList[indexPath.row].length)
+        if let img = Music.dummyMusicList[indexPath.row].albumImg{
+            cell.albumImg.image = img
+        }
+        
         return cell
     }
+    
 }
